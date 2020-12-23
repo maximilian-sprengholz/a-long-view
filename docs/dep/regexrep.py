d28c6de64421a98188a3d2efd8ec55c00646cb80
Maximilian Sprengholz <maximilian.sprengholz@hu-berlin.de>
2020-12-23 02:18:16 +0100

    regex fix

--- docs/dep/regexrep.py
+++ docs/dep/regexrep.py
@@ -2,8 +2,12 @@
 import sys
 import re
 
+replace_string = ''
+for i in xrange(3,len(sys.argv)):
+    replace_string += sys.argv[i]
+
 with open(sys.argv[1],'r') as file:
     filedata = file.read()
-    filedata = re.sub(sys.argv[2], sys.argv[3], filedata)
+    filedata = re.sub(sys.argv[2], replace_string, filedata)
 with open(sys.argv[1],'w') as file:
     file.write(filedata)
